a20becc3414e79e7fe9fd54245f1e58b685a39ea
Douglas Ernandes <DouglasErnandes87@gmail.com>
2025-10-03 16:36:26 +0000

    O guia que o usuário vai rolando a tela está demorando de aparecer, encu

--- src/components/landing/storytelling.tsx
+++ src/components/landing/storytelling.tsx
@@ -21,8 +21,8 @@ export function Storytelling() {
         });
       },
       {
-        threshold: 0.2,
-        rootMargin: "0px 0px -100px 0px",
+        threshold: 0.1,
+        rootMargin: "0px",
       }
     );
 
